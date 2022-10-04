875bf0334282388b7b4d7a7493640cdd753c7a36
TobiasMiosczka <6351397+tobiasmiosczka@users.noreply.github.com>
2022-10-04 21:40:40 +0200

    [NO-TICKET] Fixed a bug where the most recent training was not always returned

--- src/main/java/nami/connector/NamiUtil.java
+++ src/main/java/nami/connector/NamiUtil.java
@@ -4,15 +4,24 @@ import nami.connector.namitypes.NamiBaustein;
 import nami.connector.namitypes.NamiSchulung;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class NamiUtil {
 
-    //TODO: fix
+    /**
+     * Returns the latest Trainings of each Module as a Map.
+     * @param trainings collection of trainings to filter
+     * @return The latest Trainings of each Module as a Map
+     */
     public static Map<NamiBaustein, NamiSchulung> reduceToLatest(final Collection<NamiSchulung> trainings) {
-        return trainings.stream().collect(Collectors.toMap(NamiSchulung::getBaustein, Function.identity()));
+        Map<NamiBaustein, NamiSchulung> result = new HashMap<>();
+        for (NamiSchulung training : trainings) {
+            NamiBaustein module = training.getBaustein();
+            if (!result.containsKey(module) || result.get(module).getDate().isBefore(training.getDate())) {
+                result.put(module, training);
+            }
+        }
+        return result;
     }
 
 }
